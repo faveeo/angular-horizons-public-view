2bbe0983d6db01bf1acc47e82e97f0f577f4d189
Marc Escher <marc.escher@gmail.com>
2016-05-23 10:56:49 +0200

    clean

--- src/angular-horizons-public-view/directives/classicArticle/classicArticle.js
+++ src/angular-horizons-public-view/directives/classicArticle/classicArticle.js
@@ -11,7 +11,7 @@ angular.module('angularHorizonsPublicView.directives').directive('classicarticle
         data: "",
         templateUrl: "angularHorizonsPublicView/directives/classicArticle/classicArticle.tpl.html",
         replace: true,
-        controller: ['$scope', '$rootScope', '$translate', '$timeout', 'timeSinceTranslatedFilter', 'AppEvents', function ($scope, $rootScope, $translate, $timeout, timeSinceTranslated, AppEvents) {
+        controller: ['$scope', '$rootScope', '$translate', '$timeout', 'timeSinceTranslatedFilter', function ($scope, $rootScope, $translate, $timeout, timeSinceTranslated) {
             if (angular.isDefined($rootScope.assetsPath)) {
                 //remove trailing slash in the path
                 $scope.assetspath = $rootScope.assetsPath.replace(/\/$/, '');
@@ -28,17 +28,6 @@ angular.module('angularHorizonsPublicView.directives').directive('classicarticle
             $scope.addThisClass = "addthis" + new Date().getTime();
             $scope.showShareButtons = false;
             $scope.showShare = false;
-
-            //Broadcast document events
-            $scope.clickEvent = function () {
-                $rootScope.$broadcast(AppEvents.DOCUMENT_CLICK_EVENT, $scope.entry.url);
-            };
-            $scope.saveEvent = function () {
-                $rootScope.$broadcast(AppEvents.DOCUMENT_SAVE_EVENT, $scope.entry.url);
-            };
-            $scope.hideEvent = function () {
-                $rootScope.$broadcast(AppEvents.DOCUMENT_HIDE_EVENT, $scope.entry.url);
-            };
 
             $scope.hasArticle = function () {
                 return angular.isDefined($scope.entry);
