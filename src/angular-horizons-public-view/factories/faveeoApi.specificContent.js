8c72fe1ebdca2b01915d26b76e275ee55f8f5e54
Marc Escher <marc.escher@gmail.com>
2016-06-01 17:04:18 +0200

    fixes for hub

--- src/angular-horizons-public-view/factories/faveeoApi.specificContent.js
+++ src/angular-horizons-public-view/factories/faveeoApi.specificContent.js
@@ -1,59 +1,61 @@
 (function (faveeoApi) {
 
 
-    faveeoApi.factory('FaveeoApiSpecificContent', ['$q', 'Restangular', function ($q, Restangular) {
-        var factory = {};
-        factory.path = "twitterinfluencers2/";
-        factory.restangularAPI = Restangular.all(factory.path);
+	faveeoApi.factory('FaveeoApiSpecificContent', ['$q', 'Restangular', 'HttpErrorHandler', function ($q, Restangular, HttpErrorHandler) {
+		var factory = {};
+		factory.path = "twitterinfluencers2/";
+		factory.restangularAPI = Restangular.all(factory.path);
 
-        /**
-         * Get influencers content
-         * @param socialMagazineId
-         * @param dayStart Number of days to skip before getting content
-         * @param dayRange Number of days to get content for
-         * @param page
-         * @param pageSize
-         * @param forceRefresh To require content to be computed, breaking the cache of the API
-         * @param successCallback
-         * @param errorCallback
-         */
-        factory.getContent = function (socialMagazineId, dayStart, dayRange, page, pageSize, forceRefresh, successCallback, errorCallback) {
+		/**
+		 * Get influencers content
+		 * @param socialMagazineId
+		 * @param dayStart Number of days to skip before getting content
+		 * @param dayRange Number of days to get content for
+		 * @param page
+		 * @param pageSize
+		 * @param forceRefresh To require content to be computed, breaking the cache of the API
+		 * @param successCallback
+		 * @param errorCallback
+		 */
+		factory.getContent = function (socialMagazineId, dayStart, dayRange, page, pageSize, forceRefresh, successCallback, errorCallback) {
 
-            var queryParams = {
-                page: page,
-                pagesize: pageSize
-            };
+			var queryParams = {
+				page: page,
+				pagesize: pageSize
+			};
 
-            var dayStartInt = parseInt(dayStart);
-            if (dayStartInt > 0) {
-                queryParams.to = "d" + dayStartInt;
-            }
-            var dayRangeInt = parseInt(dayRange);
-            if (dayRangeInt > 0) {
-                queryParams.from = "d" + (dayRangeInt + dayStartInt);
-            }
+			var dayStartInt = parseInt(dayStart);
+			if (dayStartInt > 0) {
+				queryParams.to = "d" + dayStartInt;
+			}
+			var dayRangeInt = parseInt(dayRange);
+			if (dayRangeInt > 0) {
+				queryParams.from = "d" + (dayRangeInt + dayStartInt);
+			}
 
-            // If requested add a cache breaker parameter with a random value
-            if (forceRefresh === true) {
-                queryParams.cb = new Date().getTime() * (Math.random() + 1);
-            }
+			// If requested add a cache breaker parameter with a random value
+			if (forceRefresh === true) {
+				queryParams.cb = new Date().getTime() * (Math.random() + 1);
+			}
 
-            factory.restangularAPI.withHttpConfig({timeout: factory.getContentDeferred.promise}).customGET(socialMagazineId + "/content", queryParams).then(
-                successCallback,
-                HttpErrorHandler.useDefaultCallbackIfUndefined(errorCallback)
-            );
-        };
+			factory.restangularAPI.withHttpConfig({timeout: factory.getContentDeferred.promise}).customGET(socialMagazineId + "/content", queryParams).then(
+				function (data) {
+					successCallback(data.content);
+				},
+				HttpErrorHandler.useDefaultCallbackIfUndefined(errorCallback)
+			);
+		};
 
-        // gives the ability to cancel all ongoing getContent queries
-        // used when changing filter / date while staying in the same page
-        factory.getContentDeferred = $q.defer();
-        factory.abortGetContent = function () {
-            factory.getContentDeferred.resolve();
-            factory.getContentDeferred = $q.defer();
-        };
+		// gives the ability to cancel all ongoing getContent queries
+		// used when changing filter / date while staying in the same page
+		factory.getContentDeferred = $q.defer();
+		factory.abortGetContent = function () {
+			factory.getContentDeferred.resolve();
+			factory.getContentDeferred = $q.defer();
+		};
 
-        return factory;
-    }]);
+		return factory;
+	}]);
 
 
 }(angular.module("angularHorizonsPublicView.faveeoApi")));
